71fe5d1b0660a63827f91a616e3e718d47111cb5
NoaFeldman <feld.noa@gmail.com>
2018-01-25 12:12:19 +0200

    pass variables instead of array

--- MPS/plotRDMSpectrum.m
+++ MPS/plotRDMSpectrum.m
@@ -1,5 +1,6 @@
-function plotRDMSpectrum(N, Delta)
+function plotRDMSpectrum(N, Delta1, Delta2, Delta3, Delta4)
     tic;
+    Delta = [Delta1, Delta2, Delta3, Delta4];
     % Recreate plot fFig.2 from https://arxiv.org/pdf/1407.3779.pdf
     for d = 1:length(Delta)
         [psi, H, HR, HL] = myStartup(N, 0, 1, Delta(d));
@@ -21,25 +22,23 @@ function plotRDMSpectrum(N, Delta)
         end
         [~, ~, I] = orthoQS(M, [1, 2], '<<', opts{:});
         b = -log(I.svd(1));
-        x = zeros(length(I.svd));
-        y = zeros(length(I.svd));
-        for i = 1:length(x)
-            x(d, i) = 2 * sqrt(b * log(I.svd(1) / I.svd(i)));
-            y(d, i) = i - 1;
+        for i = 1:length(I.svd)
+            x(i) = 2 * sqrt(b * log(I.svd(1) / I.svd(i)));
+            y(i) = i - 1;
             if (x(i) > 10)
                 break;
             end
         end
+        hold on;
+        plot(x, y);
         legendInfo{d} = ['Delta = ' num2str(Delta(d))];
         disp(strcat('Finished calculating GS and spectrum for lambda = ', num2str(Delta(d))));
         toc;
     end
-    hold on;
     for d = 1:length(Delta)
-        plot(x(d, :), y(d, :));
-        xlabel('2$\sqrt{bln(\frac{\lambda_{max}}{\lambda})}$', 'Interpreter', 'latex');
-        ylabel('n($\lambda$)', 'Interpreter', 'latex');
     end
+    xlabel('2$\sqrt{bln(\frac{\lambda_{max}}{\lambda})}$', 'Interpreter', 'latex');
+    ylabel('n($\lambda$)', 'Interpreter', 'latex');
     for i= 1 : 1e3
         clx(i) = i / 100;
         cly(i) = besseli(0, (clx(i) / 2)^2);
