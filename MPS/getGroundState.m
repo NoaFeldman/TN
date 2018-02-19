a80f66b7dce4566881a585d92c6516400cc0f9b8
NoaFeldman <feld.noa@gmail.com>
2018-02-19 17:03:30 +0200

    debug prints

--- MPS/getGroundState.m
+++ MPS/getGroundState.m
@@ -3,6 +3,9 @@ function [psi, H, HR, HL] = getGroundState(N, h, JPM, JZ, m, opts)
     % Find ground state
     ECurr = 0;
     for i=1:100
+        disp(strcat('sweep num = ', int2str(i)));
+        psi(length(psi) - 1)
+        psi(length(psi)
         EForm = ECurr;
         [HL, HR, psi, ~] = dmrgSweep(HL, HR, H, psi, '<<', opts);
         [HL, HR, psi, ECurr] = dmrgSweep(HL, HR, H, psi, '>>', opts);
