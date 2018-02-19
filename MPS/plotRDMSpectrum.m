3ffec9bafbb6b30e829962bb534a08c1b65979d3
NoaFeldman <feld.noa@gmail.com>
2018-02-19 14:55:06 +0200

    tst

--- MPS/plotRDMSpectrum.m
+++ MPS/plotRDMSpectrum.m
@@ -1,9 +1,14 @@
 function plotRDMSpectrum(N, Delta1, Delta2, Delta3, Delta4)
     tic;
     Delta = [Delta1, Delta2, Delta3, Delta4];
-    % Recreate plot fFig.2 from https://arxiv.org/pdf/1407.3779.pdf
+    mMax = 8;
+    weights = zeros(length(Delta), mMax + 1);
+    statesNums = zeros(length(Delta), mMax + 1);
+    % Recreate Fig.2 from https://arxiv.org/pdf/1407.3779.pdf
+    % Added data for Fig.4. If this doesn't work, try: 
+    % Verifying l of A, 
     for d = 1:length(Delta)
-        [psi, H, HR, HL] = myStartup(N, 0, 1, Delta(d));
+        [psi, H, HR, HL] = myStartup(N, 0, 1, Delta(d), 0);
         % Find ground state
         ECurr = 0;
         opts = {'Nkeep', 100};
@@ -15,39 +20,72 @@ function plotRDMSpectrum(N, Delta1, Delta2, Delta3, Delta4)
                 break;
             end
         end
-        % Sweep to mid chain 
-        i = length(psi);
-        while(i > floor(length(psi)/2))
+        eigenstatesCounter = 0;
+        while(i > floor(length(psi)/2) + 1)    
             [HL, HR, psi, E, i, M] = dmrgStep(HL, HR, H, psi, i, '<<', opts);
         end
-        [~, ~, I] = orthoQS(M, [1, 2], '<<', opts{:});
-        b = -log(I.svd(1));
-        for i = 1:length(I.svd)
-            x(i) = 2 * sqrt(b * log(I.svd(1) / I.svd(i)));
-            y(i) = i - 1;
-            if (x(i) > 10)
+        for m = 0:mMax
+            % Sweep to mid chain 
+            i = length(psi);
+            % force S^z(A) = m
+            projector = getProjector(psi(length(psi)/2), m);
+            temp = psi(length(psi)/2);
+            psi(length(psi)/2) = contract(psi(length(psi)/2), 3, projector, 1);
+            if (isempty(psi(length(psi)/2)))
+                psi(length(psi)/2) = temp;
                 break;
             end
+            [~, ~, ~, ~, i, M] = dmrgStep(HL, HR, H, psi, i, '<<', opts);
+            [~, ~, I] = orthoQS(M, [1, 2], '<<', opts{:});
+            b = -log(I.svd(1));
+            weight = 0;
+            for i = 1:length(I.svd)
+                if (I.svd(i) < 1e-9)
+                    break;
+                end
+                eigenstatesCounter = eigenstatesCounter + 1;
+                weight = weight + I.svd(i);
+                x(eigenstatesCounter) = 2 * sqrt(b * log(I.svd(1) / I.svd(i)));
+            end
+            statesNums(d, m+1) = eigenstatesCounter;
+            weights(d, m+1) = weight;
+            psi(length(psi)/2) = temp;
         end
-        hold on;
-        plot(x, y);
-        legendInfo{d} = ['Delta = ' num2str(Delta(d))];
-        disp(strcat('Finished calculating GS and spectrum for lambda = ', num2str(Delta(d))));
+%        disp(strcat('Finished calculating GS and spectrum for lambda = ', num2str(Delta(d))));
         toc;
     end
-    for d = 1:length(Delta)
-    end
-    xlabel('2$\sqrt{bln(\frac{\lambda_{max}}{\lambda})}$', 'Interpreter', 'latex');
-    ylabel('n($\lambda$)', 'Interpreter', 'latex');
-    for i= 1 : 1e3
-        clx(i) = i / 100;
-        cly(i) = besseli(0, (clx(i) / 2)^2);
-        legendInfo{length(Delta) + 1} = ['CL'];
-    end
-    plot(clx, cly);
-    legend(legendInfo);
-    set(gca, 'YScale', 'log');
-    savefig('RDMSpec.fig');
-    hold off;
+%     xlabel('2$\sqrt{bln(\frac{\lambda_{max}}{\lambda})}$', 'Interpreter', 'latex');
+%     ylabel('n($\lambda$)', 'Interpreter', 'latex');
+%     for i= 1 : 1e3
+%         clx(i) = i / 100;
+%         cly(i) = besseli(0, (clx(i) / 2)^2);
+%         legendInfo{length(Delta) + 1} = ['CL'];
+%     end
+%     plot(clx, cly);
+%     legend(legendInfo);
+%     set(gca, 'YScale', 'log');
+%     savefig('RDMSpec.fig');
+%     hold off;
+    save('fig4data', 'weights', 'statesNums', 'x');
 end
     
+function projector = getProjector(mps, m)
+    %
+    id = getIdentity(mps, 3);
+    mInd = 0;
+    for i = 1 : length(id.Q{1})
+        if (id.Q{1}(i) == m*2)
+            mInd = i;
+            break;
+        end
+    end
+    if (mInd == 0)
+        projector = QSpace;
+        return;
+    end
+    projector = id;
+    for i = 1 : length(projector.Q)
+        projector.Q{i} = projector.Q{i}(mInd);
+    end
+    projector.data = projector.data(mInd);
+end
