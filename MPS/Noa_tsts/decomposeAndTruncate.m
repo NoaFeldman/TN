8da1ecdfd613723c6264b858bc5ae8b0ee3d7228
NoaFeldman <feld.noa@gmail.com>
2018-02-19 16:56:02 +0200

    debug prints

--- MPS/Noa_tsts/decomposeAndTruncate.m
+++ MPS/Noa_tsts/decomposeAndTruncate.m
@@ -2,13 +2,6 @@ function [psi, truncErr] = decomposeAndTruncate(M, k, psi, dir, opts)
     % perform an SVD decomposition on M.
     % Assign results to psi(k), psi(k+1).    
     [psi(k), psi(k+1), I] = orthoQS(M, [1, 2], dir, opts{:});
-    disp(strcat('k = ', int2str(k)));
-    disp('M = ');
-    disp(M);
-    disp('psi(k) = ');
-    disp(psi(k));
-    disp('psi(k+1) = ');
-    disp(psi(k+1));
     truncErr = I.svd2tr;
     psi(k).info.itags(3) = strcat(int2str(k), 'a', psi(k).info.itags(3));
     psi(k+1).info.itags(1) = strcat(int2str(k), 'a', psi(k+1).info.itags(1));
