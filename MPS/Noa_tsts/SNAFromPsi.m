433d7a99e8117eefe2e1ee8a59ee4d0056c7df01
NoaFeldman <feld.noa@gmail.com>
2018-03-08 14:33:53 +0200

    minor bug

--- MPS/Noa_tsts/SNAFromPsi.m
+++ MPS/Noa_tsts/SNAFromPsi.m
@@ -1,11 +1,13 @@
 function SNAFromPsi(psi, spectrumFileName, figFileName)
     saveRDMSpectrum(spectrumFileName, psi);
-    spectrum = load(spectrumFileName);
-    sz = keys(spectrum);
-    val = values(spectrum);
+    s = load(spectrumFileName);
+    sz = keys(s.spectrum);
+    val = values(s.spectrum);
+    x = zeros(1, length(sz));
+    s = zeros(1, length(sz));
     for i = 1:length(sz)
         x(i) = str2num(sz{i});
-        s(i) = -sum(val{i}.*log(val{i}));
+        s(i) = -sum(val{i}.*log2(val{i}));
     end
     STheo = sqrt(pi * log(length(psi)) / 2) / 3 .* exp(-1 * pi^2 .* (x/2).^2 ./ (2 * log(length(psi))));
     scatter(x(:), s(:), 'markerEdgeColor', 'r')
