ea715bf822b434cb731e18f70094aa97d06171f8
NoaFeldman <feld.noa@gmail.com>
2018-04-02 14:17:07 +0300

    reverse trotter, test for <N_A> != 0 issue

--- MPS/Noa_tsts/trotterSweep.m
+++ MPS/Noa_tsts/trotterSweep.m
@@ -1,17 +1,38 @@
 function [psi, truncErr] = trotterSweep(psi, dtReal, dtIm, H, opts)
     % 
     truncErr = 0;
+%     trotterGates = getTrotterGates(H, dtReal, dtIm);
+%     for k = length(psi) - 1 : -1 : 1
+%         [err, psi] = applyHPair(trotterGates, k, psi, '<<', opts);
+%         if (err > truncErr)
+%             truncErr = err;
+%         end
+%     end
+%     for k = 1 : length(psi) - 1
+%         [err, psi] = applyHPair(trotterGates, k, psi, '>>', opts);
+%         if (err > truncErr)
+%             truncErr = err;
+%         end
+%     end
+%     TODO just a test to see what causes <N_A> shift from 0. 
+    workingSiteIndex = length(psi);
+    while workingSiteIndex > 1
+        [psi, workingSiteIndex] = shiftWorkingSite(psi, workingSiteIndex, '<<');
+    end
     trotterGates = getTrotterGates(H, dtReal, dtIm);
+    for k = 1 : length(psi) - 1
+        [err, psi] = applyHPair(trotterGates, k, psi, '>>', opts);
+        if (err > truncErr)
+            truncErr = err;
+        end
+    end
     for k = length(psi) - 1 : -1 : 1
         [err, psi] = applyHPair(trotterGates, k, psi, '<<', opts);
         if (err > truncErr)
             truncErr = err;
         end
     end
-    for k = 1 : length(psi) - 1
-        [err, psi] = applyHPair(trotterGates, k, psi, '>>', opts);
-        if (err > truncErr)
-            truncErr = err;
-        end
+    while workingSiteIndex < length(psi) 
+        [psi, workingSiteIndex] = shiftWorkingSite(psi, workingSiteIndex, '>>');
     end
 end
