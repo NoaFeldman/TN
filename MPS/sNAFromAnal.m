f933872802eb72b5fce91cd5cb4844810e8b0f8d
NoaFeldman <feld.noa@gmail.com>
2018-04-11 10:53:57 +0300

    time debugging for analytical

--- MPS/sNAFromAnal.m
+++ MPS/sNAFromAnal.m
@@ -1,7 +1,8 @@
-function sNAFromAnal(L, tStep, tStepNum, filename)
+function sNAFromAnal(L, tStep, tStepNum)
     path(path, [pwd, '/MPSPACK_v3.0']);
     path(path, [pwd, '/Noa_tsts']);
     startup;
+    tic;
     % TODO take to outside func
     ckcqA = zeros(L/2, L/2);
     for i = 1 : L/4 % L/4 + 1 : L/2
@@ -12,39 +13,39 @@ function sNAFromAnal(L, tStep, tStepNum, filename)
     cicj = zeros(L, L);
     cicj(1:L/2, 1:L/2) = cicjA;
     cicj(L/2 + 1 : L, L/2 + 1 : L) = cicjA;
-
+    disp('got cicj');
+    toc;
+    
     x = L / 4 - 2 : L / 4 + 2;
     s = zeros(length(x), tStepNum + 1);
     p = zeros(length(x), tStepNum + 1);
     sFull = zeros(1, tStepNum + 1);
     U = realSpaceToDualSpace(L);
+    disp('got U');
+    toc;
     for step = 0 : tStepNum
         [~, v] = eig(cicj(1:L/2, 1:L/2));
         for i = 1 : length(v)
             f(i) = v(i, i);
         end
+        disp(strcat('step = ', num2str(step), ', line 31'));
+        toc;
         p(:, step + 1)  = getSNA(1, f, x, L);
+        disp(strcat('step = ', num2str(step), ', line 34'));
+        toc;
         s(:, step + 1)  = getEE(f, x, L);
         sFull(step + 1) = sum(s(:, step + 1));
+        disp(strcat('step = ', num2str(step), ', line 38'));
+        toc;
         ckcq = U' * cicj * U;
         ckcq = expectedCkCqMatrix(L, ckcq, tStep);
         cicj = U * ckcq * U';
+        disp(strcat('step = ', num2str(step)));
+        toc;
     end 
     t = 0 : tStepNum;
     t = t * tStep;
-%     plot(t, sFull);
-    hold on
-%     legendInfo{1} = 'sFull';
-    for i = 1 : length(x)
-        plot(t(:), p(i, :));
-        legendInfo{i} = strcat('$s^z = $', num2str(L/4 - x(i)));
-        hold on
-    end
-    legend(legendInfo, 'Interpreter', 'latex');
-    set(gca, 'XScale', 'log');
-    savefig(filename);
-    save(strcat('theoSP', int2str(L)), 's', 'p', 'sFull', 't');
-    hold off
+    save(strcat('theoSP', int2str(L), '_', num2str(tStepNum)), 's', 'p', 'sFull', 't');
 end
 
 function S = getEE(f, x, L)
