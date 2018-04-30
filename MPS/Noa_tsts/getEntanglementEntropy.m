83c61ce9058965373ff8036aa74669b13835d59b
NoaFeldman <feld.noa@gmail.com>
2018-04-30 15:59:53 +0300

    minor fixes

--- MPS/Noa_tsts/getEntanglementEntropy.m
+++ MPS/Noa_tsts/getEntanglementEntropy.m
@@ -1,7 +1,7 @@
-function SA = getEntanglementEntropy(t, a)
+function SA = getEntanglementEntropy(t, a, L)
     % fitfun for fitgui/fitnonlin.
     % based on Eq. (89) in https://arxiv.org/pdf/0905.4013.pdf
     % and Eq. (36) in https://arxiv.org/pdf/1105.4846.pdf
     c = 1;
-    SA = - c / 6 .* log(getOnePointFunc(t, a(1))) + a(2);
+    SA = - c / 6 .* log(getOnePointFunc(t, a(1), L)) + a(2);
 end
--- MPS/Noa_tsts/getOnePointFunc.m
+++ MPS/Noa_tsts/getOnePointFunc.m
@@ -1,8 +1,6 @@
-function X = getOnePointFunc(t, epsilon)
-    L = 600;
+function X = getOnePointFunc(t, epsilon, L)
     vF = 2;
     X = abs(pi .* sinh(pi/L * 2 * epsilon) ./ ...
         (2 .* sinh(pi / L .* (epsilon + i .* vF .* t)) .* ...
          sinh(pi / L .* (epsilon - i.*vF.*t))));
-%     X = (t.^2 + epsilon^2) / (2*epsilon);
 end
