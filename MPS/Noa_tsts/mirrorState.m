478ef66536e440277182c123b7777f66388558a4
NoaFeldman <feld.noa@gmail.com>
2018-05-03 10:05:55 +0300

    mirror subsystem B

--- MPS/Noa_tsts/mirrorState.m
+++ MPS/Noa_tsts/mirrorState.m
@@ -1,14 +1,11 @@
 function mirror = mirrorState(psi)
     % Assuming psi is left cannonicized.
-    % Currently only working for U)1) symmetry.
+    % Not handling mirror.info.itags.
+    % Currently only works 
     mirror = QSpace(length(psi));
     for i = 1:length(psi)
         mirrorInd = length(psi) + 1 - i;
-        mirror(mirrorInd) = psi(i);
-        mirror(mirrorInd).info.itags{3} = psi(i).info.itags{1};
-        mirror(mirrorInd).info.itags{1} = psi(i).info.itags{3};
-        mirror(mirrorInd).Q{3} = psi(i).Q{1};
-        mirror(mirrorInd).Q{1} = psi(i).Q{3};
+        mirror(mirrorInd) = permute(psi(i), length(psi(i).Q):-1:1);
     end
 end
     
