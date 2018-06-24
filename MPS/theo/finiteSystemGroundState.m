6ebef7111310e99a8ab56c898ae2478d3649eeb3
Feldman Noa <feld.noa@gmail.com>
2018-06-24 17:44:14 +0300

    ratios

--- MPS/theo/finiteSystemGroundState.m
+++ MPS/theo/finiteSystemGroundState.m
@@ -1,4 +1,4 @@
-function finiteSystemGroundState(Ls, filename, ratio)
+function finiteSystemGroundState(Ls, filename, ratio1, ratio2)
     chargeRange = -5:5;
     s = zeros(length(chargeRange), length(Ls));
     p = zeros(length(chargeRange), length(Ls));
@@ -7,9 +7,9 @@ function finiteSystemGroundState(Ls, filename, ratio)
     gaussian = fittype('sqrt(1/(2*pi*c))*exp(-(x-b)^2/(2*c))', 'independent', 'x', 'dependent', 'y');
     for l = 1:length(Ls)
          L = Ls(l);
-         x = L * ratio / 2 + chargeRange;
+         x = L * (ratio2 - ratio1) / 2 + chargeRange;
          cicj = getCiCj0Matrix(L);
-         [~, v] = eig(cicj(1:L*ratio, 1:L*ratio));
+         [~, v] = eig(cicj((L*ratio1+1):L*ratio2, (L*ratio1+1):L*ratio2));
          f = zeros(1, length(v));
          for i = 1 : length(v)
              f(i) = v(i, i);
@@ -17,7 +17,7 @@ function finiteSystemGroundState(Ls, filename, ratio)
          p(:, l)  = getSNA(1, f, x, L);
          s(:, l)  = getEE(f, x, L);
          sFull(l) = sum(s(:, l));
-         fg = fit(x.', real(p(:, l)), gaussian, 'StartPoint', [L * ratio / 2 0.1]);
+         fg = fit(x.', real(p(:, l)), gaussian, 'StartPoint', [L * (ratio1 - ratio2) / 2 0.1]);
          sigma2(l) = fg.c;
     end
     save(filename, 'Ls', 'p', 's', 'sFull', 'sigma2');
