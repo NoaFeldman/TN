299f8306b6ff871b5981038f7905dd0c63ff666a
feld.noa@gmail.com <feld.noa@gmail.com>
2018-06-24 17:23:45 +0300

    some order

--- MPS/theo/fitSTheo.m
+++ MPS/theo/fitSTheo.m
@@ -31,9 +31,9 @@ function fitSTheo(sz, t, fitStartInd, fitEndInd, p, sFull, s, filename, figname,
     hold on;
     plot(t, s(round(length(sz) / 2) + 1, :), 'color', 'c');
     plot(t, s(round(length(sz) / 2) + 2, :), 'color', 'c');
-    plot(t, stheo(t, [epsP constP epsS c1], [L 0]), 'color', 'b');
-    plot(t, stheo(t, [epsP constP epsS c1], [L 2]), 'color', 'b');
-    plot(t, stheo(t, [epsP constP epsS c1], [L 4]), 'color', 'b');
+    plot(t, timeDependentSTheo(t, [epsP constP epsS c1], [L 0]), 'color', 'b');
+    plot(t, timeDependentSTheo(t, [epsP constP epsS c1], [L 1]), 'color', 'b');
+    plot(t, timeDependentSTheo(t, [epsP constP epsS c1], [L 2]), 'color', 'b');
     xlabel('$t$', 'Interpreter', 'latex');
     ylabel('$S(N_A)$', 'Interpreter', 'latex');
     title('Charged resolved entanglement entropy');
