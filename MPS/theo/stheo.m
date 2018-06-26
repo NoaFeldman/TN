72c5e0fc22708f3893e64e12c2713d0ee499bbbd
feld.noa@gmail.com <feld.noa@gmail.com>
2018-06-26 12:01:50 +0300

    split stho to t or L dependent

--- MPS/theo/stheo.m
+++ MPS/theo/stheo.m
@@ -1,25 +1,17 @@
-function s = stheo(t, params, fixed)
+function s = stheo(X_scaled, sigma2, NA)
     % S_A(N_A) = (c_n * A * B * c)'
     % c_n' |_{n = 1} = c1 (from sFull fit)
-    % A = X(t, epsS)^(c(n - 1/n)/6)
-    % B = sqrt(pi * n / 2 * K * log(X(t, epsP)) = sqrt(n / (2 * pi *
+    % A = X^(c(n - 1/n)/12)
+    % B = sqrt( n / 2 * K * pi * sigma2) = sqrt(n / (2 * pi *
     % sigma2))
     % C = e^(- n * N_A^2 / (2 * sigma2))
-    L = fixed(1);
-    NA = fixed(2);
-    epsP = params(1);
-    constP = params(2);
-    epsS = params(3);
-    c1 = params(4);
-    sigma2 = getSigmaN(t, [epsP, constP], L);
-    global c;
-    c = 1;
-    cn = 1;
-    xS = exp(-c1 * 6 / c) .* getOnePointFunc(t, epsS, L);
-%     xS = exp(-c1 * 12 / c) .* onePointFunc(t, epsS, L);
-    s = - (cn .* dnA(xS) .* B(sigma2) .* C(NA, sigma2) + ...
-        cn .* A(xS) .* dnB(sigma2) .* C(NA, sigma2) + ...
-        cn .* A(xS) .* B(sigma2) .* dnC(NA, sigma2));    
+    %
+    % Note: I take c_n = 1, and add a multiplicative constant to X_scaled
+    % detrmined by sFull.
+
+    s = - (dnA(X_scaled) .* B(sigma2) .* C(NA, sigma2) + ...
+        A(X_scaled) .* dnB(sigma2) .* C(NA, sigma2) + ...
+        A(X_scaled) .* B(sigma2) .* dnC(NA, sigma2));    
 end
     
 function a = A(x)
@@ -27,7 +19,7 @@ function a = A(x)
 end
 
 function da = dnA(x)
-    global c;
+    c = 1;
     da = c/6.* log(x);
 end
 
