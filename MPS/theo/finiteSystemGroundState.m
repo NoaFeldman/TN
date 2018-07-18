fca1dbb8907f49e6f945e6944f84012e01a455bd
feld.noa@gmail.com <feld.noa@gmail.com>
2018-07-18 10:27:01 +0300

    allow 2d graphs for u and v

--- MPS/theo/finiteSystemGroundState.m
+++ MPS/theo/finiteSystemGroundState.m
@@ -4,6 +4,8 @@ function finiteSystemGroundState(Ls, filename, ratio1s, ratio2s)
 %     p = zeros(length(chargeRange), length(Ls));
 %     sigma2 = zeros(1, length(Ls));
 %     sFull = zeros(1, length(Ls)); 
+    ratios = ratio2s - ratio1s.';
+    ratios(ratios < 0) = 0;
     s = zeros(length(chargeRange), length(ratio1s), length(ratio2s));
     p = zeros(length(chargeRange), length(ratio1s), length(ratio2s));
     sigma2 = zeros(length(ratio1s), length(ratio2s));
@@ -11,7 +13,7 @@ function finiteSystemGroundState(Ls, filename, ratio1s, ratio2s)
     gaussian = fittype('sqrt(1/(2*pi*c))*exp(-(x-b)^2/(2*c))', 'independent', 'x', 'dependent', 'y');
 %     for l = 1:length(Ls)
         for l1 = 1:length(ratio1s)
-            for l2 = 1:length(ratio1s)
+            for l2 = 1:length(ratio2s)
                 ratio1 = ratio1s(l1);
                 ratio2 = ratio2s(l2);
                 if (ratio2 > ratio1)
@@ -27,12 +29,12 @@ function finiteSystemGroundState(Ls, filename, ratio1s, ratio2s)
                     p(:, l1, l2)  = getSNA(1, f, x, L);
                     s(:, l1, l2)  = getEE(f, x, L);
                     sFull(l1, l2) = sum(s(:, l1, l2));
-                    fg = fit(x.', real(p(:, l1, l2)), gaussian, 'StartPoint', [L * (ratio1 - ratio2) / 2 0.1]);
+                    fg = fit(x.', real(p(:, l1, l2)), gaussian, 'StartPoint', [L * (ratio2 - ratio1) / 2 0.1]);
                     sigma2(l1, l2) = fg.c;
                 end
             end
         end
 %     end
 %     save(filename, 'Ls', 'p', 's', 'sFull', 'sigma2');
-    save(filename, 'p', 's', 'sFull', 'sigma2', 'ratio1s', 'ratio2s');
+    save(filename, 'p', 's', 'sFull', 'sigma2', 'ratios', 'ratio1s', 'ratio2s');
 end
