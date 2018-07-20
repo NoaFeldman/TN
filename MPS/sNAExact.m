b59e4e8a13c34b5800a5f75124adbe38f3067030
NoaFeldman <feld.noa@gmail.com>
2018-07-20 10:25:48 +0300

    allow for mid chain sumulation

--- MPS/sNAExact.m
+++ MPS/sNAExact.m
@@ -1,4 +1,4 @@
-function sNAExact(L, LA, tFirstStep, tStep, tStepNum, fileNameAddition)
+function sNAExact(L, u, v, tFirstStep, tStep, tStepNum, fileNameAddition)
     path(path, [pwd, '/theo']);
     
     cicjA = getCiCj0Matrix(L/2);
@@ -6,7 +6,7 @@ function sNAExact(L, LA, tFirstStep, tStep, tStepNum, fileNameAddition)
     cicj(1:L/2, 1:L/2) = cicjA;
     cicj(L/2 + 1 : L, L/2 + 1 : L) = cicjA;
     
-    x = LA / 2 - 10 : 2 : LA / 2 + 10;
+    x = (v-u) / 2 - 10 : 2 : (v-u) / 2 + 10;
     s = zeros(length(x), tStepNum + 1);
     p = zeros(length(x), tStepNum + 1);
     sFull = zeros(1, tStepNum + 1);
@@ -16,18 +16,13 @@ function sNAExact(L, LA, tFirstStep, tStep, tStepNum, fileNameAddition)
     ckcq = expectedCkCqMatrix(L, ckcq, tFirstStep);
     cicj = U * ckcq * U';
     for step = tFirstStep : tStepNum
-        [~, v] = eig(cicj(1:LA, 1:LA));
-        for i = 1 : length(v)
-            f(i) = v(i, i);
+        [~, V] = eig(cicj(u+1:v, u+1:v));
+        for i = 1 : length(V)
+            f(i) = V(i, i);
         end
         p(:, step + 1)  = getSNA(1, f, x, L);
         s(:, step + 1)  = getEE(f, x, L);
         sFull(step + 1) = sum(s(:, step + 1));
-        
-        [~, v] = eig(cicj(LA+1:L, LA+1:L));
-        for i = 1 : length(v)
-            f(i) = v(i, i);
-        end
        
         ckcq = U' * cicj * U;
         ckcq = expectedCkCqMatrix(L, ckcq, tStep);
@@ -40,6 +35,6 @@ function sNAExact(L, LA, tFirstStep, tStep, tStepNum, fileNameAddition)
     end 
     t = 0 : tStepNum;
     t = t * tStep;
-    save(strcat('theoSP', int2str(L), '_', int2str(LA), '_', num2str(tStepNum), '_', fileNameAddition)...
+    save(strcat('theoSP', int2str(L), '_', int2str(u), '-', int2str(v), '_', num2str(tStep*tStepNum), '_', fileNameAddition)...
         , 't', 's', 'p', 'sFull');
 end
