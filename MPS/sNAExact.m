543b8addb228000152f755cc88a8c6939796b49b
feld.noa@gmail.com <feld.noa@gmail.com>
2018-08-06 09:53:06 +0300

    analyze s(alpha) in time dependent case

--- MPS/sNAExact.m
+++ MPS/sNAExact.m
@@ -1,4 +1,4 @@
-function sNAExact(L, u, v, tFirstStep, tStep, tStepNum, fileNameAddition)
+function sNAExact(L, u, v, tFirstStep, tStep, tStepNum, fileNameAddition, alphas)
     path(path, [pwd, '/theo']);
     
     cicjA = getCiCj0Matrix(L/2);
@@ -8,7 +8,16 @@ function sNAExact(L, u, v, tFirstStep, tStep, tStepNum, fileNameAddition)
     
     x = (v-u) / 2 - 10 : 2 : (v-u) / 2 + 10;
     s = zeros(length(x), tStepNum + 1);
-    p = zeros(length(x), tStepNum + 1);
+    s1 = zeros(length(x), tStepNum + 1);
+    s2 = zeros(length(x), tStepNum + 1);
+    s3 = zeros(length(x), tStepNum + 1);
+    s4 = zeros(length(x), tStepNum + 1);
+    s5 = zeros(length(x), tStepNum + 1);
+    s1Alpha = zeros(length(alphas), tStepNum + 1);
+    s2Alpha = zeros(length(alphas), tStepNum + 1);
+    s3Alpha = zeros(length(alphas), tStepNum + 1);
+    s4Alpha = zeros(length(alphas), tStepNum + 1);
+    s5Alpha = zeros(length(alphas), tStepNum + 1);
     sFull = zeros(1, tStepNum + 1);
     
   
@@ -21,7 +30,16 @@ function sNAExact(L, u, v, tFirstStep, tStep, tStepNum, fileNameAddition)
         for i = 1 : length(V)
             f(i) = V(i, i);
         end
-        p(:, step + 1)  = getSNA(1, f, x, L);
+        s1(:, step + 1)  = getSNA(1, f, x, L);
+        s2(:, step + 1)  = getSNA(2, f, x, L);
+        s3(:, step + 1)  = getSNA(3, f, x, L);
+        s4(:, step + 1)  = getSNA(4, f, x, L);
+        s5(:, step + 1)  = getSNA(5, f, x, L);
+        s1Alpha(:, step + 1) = getSAlpha(1, alphas, f, 1);
+        s2Alpha(:, step + 1) = getSAlpha(2, alphas, f, 1);
+        s3Alpha(:, step + 1) = getSAlpha(3, alphas, f, 1);
+        s4Alpha(:, step + 1) = getSAlpha(4, alphas, f, 1);
+        s5Alpha(:, step + 1) = getSAlpha(5, alphas, f, 1);
         s(:, step + 1)  = getEE(f, x, L);
         sFull(step + 1) = sum(s(:, step + 1));
         
@@ -32,5 +50,6 @@ function sNAExact(L, u, v, tFirstStep, tStep, tStepNum, fileNameAddition)
     t = 0 : tStepNum;
     t = t * tStep;
     save(strcat('theoSP', int2str(L), '_', int2str(u), '-', int2str(v), '_', num2str(tStep*tStepNum), '_', fileNameAddition)...
-        , 't', 's', 'p', 'sFull');
+        , 't', 's', 's1', 's2', 's3', 's4', 's5', ...
+        'alphas', 's1Alpha', 's2Alpha', 's3Alpha', 's4Alpha', 's5Alpha', 'sFull');
 end
