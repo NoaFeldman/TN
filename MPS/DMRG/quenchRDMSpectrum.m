046ea7d2b503b8ea530ac08f94ef592868aba7e2
NoaFeldman <feld.noa@gmail.com>
2018-08-29 18:13:59 +0200

    picking up bug

--- MPS/DMRG/quenchRDMSpectrum.m
+++ MPS/DMRG/quenchRDMSpectrum.m
@@ -1,4 +1,4 @@
-function quenchRDMSpectrum(L, h, JPM, JZ, m, dt, tStepOverDt, firstStep, stepsNum, dirName, subsystemA)
+function quenchRDMSpectrum(L, h, JPM, JZ, m, dt, tStep, tFirstStep, tStepsNum, dirName, subsystemA)
     % Saves the eigenvalues of the RDM (of half the lattice) for two ground
     % state L/2 lattices suddenly coupled.
     % We first calculate g.s of an L/2 lattice.
@@ -7,41 +7,50 @@ function quenchRDMSpectrum(L, h, JPM, JZ, m, dt, tStepOverDt, firstStep, stepsNu
     % Every t = tStep * dt we save the spectrum of the RDM.
     % h, JPM, JZ - Heisenberg hamiltonian parameters.
     % m = initial spin of state (assumed spin of g.s) for the L / 2 chain.
-    path(path, ['/home/noa/TN/MPS/MPSPACK_v3.0']);
-    path(path, ['/home/noa/TN/MPS/DMRG']);
+    path(path, [pwd, '/MPSPACK_v3.0']);
+    path(path, [pwd, '/DMRG']);
+    if nargin == 10
+        subsystemA = 'half';
+    end
     startup;
     tic;
-    topts = {'Nkeep', 1024, 'stol', 1e-8};
+    topts = {'Nkeep', 10};
 %     dirName = strcat('quenchSpecL', int2str(L), 'JPM', num2str(abs(JPM)), ...
 %                      'JZ', num2str(abs(JZ)), 'h', num2str(abs(h)), '_200');
-    if (firstStep == 0)
+    if (tFirstStep == 0)
         [gs, ~, ~, ~] = getGroundState(L / 2, h, JPM, JZ, m);
         disp('Found ground state for L/2');
         toc;
         [~, H, ~, ~] = myStartup(L, h, JPM, JZ, m);
-        psi = coupleStates(gs, mirrorState(gs));
+        trotterGates = getTrotterGates(H, dt, 0);
         mkdir(dirName);
-        % TODO extract the saveRDMSpectrum loop for different subsystemA
-        % options and use it here
+        save(strcat(dirName, '/trotterGates.mat'), 'trotterGates');
+        psi = coupleStates(gs, mirrorState(gs));
+        saveSpec(dirName, 0, psi, subsystemA, L)
     else
-        f = load(strcat(dirName, '/psiAtStep', int2str(firstStep), '.mat'));
+        f = load(strcat(dirName, '/psiAtStep', int2str(tFirstStep - 1), '.mat'));
         psi = f.psi;
-        [~, H, ~, ~] = myStartup(L, h, JPM, JZ, m);
+        f = load(strcat(dirName, '/trotterGates.mat'));
+        trotterGates = f.trotterGates;
+%         [~, H, ~, ~] = myStartup(L, h, JPM, JZ, m);
     end
-    truncErr = zeros(1, tStepOverDt*(stepsNum - firstStep + 1));
-    trotterGates = getTrotterGates(H, dt, 0);
-    if nargin == 10
-        subsystemA = 'half';
-    end
-    for step = firstStep : stepsNum
-        if (mod(step, 1) == 0)
-            disp(strcat('saving psi at step ', int2str(step)));
-            toc;
-            save(strcat(dirName, '/psiAtStep', int2str(step), '.mat'), 'psi', 'truncErr');
-            toc;
+    truncErr = zeros(1, tStep*(tStepsNum - tFirstStep + 1));
+    
+    for step = tFirstStep : tStepsNum
+        for t = 1 : tStep
+            ind = (step - tFirstStep)*tStep + t;
+            [psi, truncErr(ind)] = trotterSweep(trotterGates, psi, topts);
         end
-        
-        if strcmp(subsystemA, 'half')
+        saveSpec(dirName, step+1, psi, subsystemA, L);
+        if (mod(step, 2) == 0)
+            save(strcat(dirName, '/psiAtStep', int2str(step), '.mat'), 'psi', 'truncErr');
+        end
+    end
+    save(strcat(dirName, '/truncErr.mat'), 'truncErr');
+end
+
+function saveSpec(dirName, step, psi, subsystemA, L)
+    if strcmp(subsystemA, 'half')
             saveRDMSpectrum(strcat(dirName, '/step', int2str(step), '.mat'), psi, length(psi)/2);
         % For the lightcone states, we take the B system to be ~L/16, so
         % the plateau will be short.
@@ -61,14 +70,5 @@ function quenchRDMSpectrum(L, h, JPM, JZ, m, dt, tStepOverDt, firstStep, stepsNu
             coupled = coupleStates(aux, psi);
             folded = foldChain(coupled);
             saveRDMSpectrum(strcat(dirName, '/step', int2str(step), '.mat'), folded, L/2);
-        end
-        
-        if (step < stepsNum)
-            for t = 1 : tStepOverDt
-                ind = (step - firstStep)*tStepOverDt + t;
-                [psi, truncErr(ind)] = trotterSweep(trotterGates, psi, topts);
-            end
-        end
     end
-    save(strcat(dirName, '/truncErr.mat'), 'truncErr');
 end
