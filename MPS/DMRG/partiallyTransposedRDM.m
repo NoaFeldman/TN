debfe883db99bbcd5a5b949186e93dac20c3a6e3
feld.noa@gmail.com <feld.noa@gmail.com>
2018-09-25 19:46:46 +0300

    more attempts

--- MPS/DMRG/partiallyTransposedRDM.m
+++ MPS/DMRG/partiallyTransposedRDM.m
@@ -4,8 +4,22 @@ function rhoT2 = partiallyTransposedRDM(N1, N2)
     M = contract(N1, 2, N2, 1);
     % This action gets me out of memory - divide to sectors
     rho = contract(M, '13', M, '13*');
-    rho = contract(contract(rho, 2, getIdentity(rho, 2, '-0', 'sA2'), '1*', [1 4 2 3]), ...
+    rho2 = contract(contract(rho, 3, getIdentity(rho, 3, '-0', 'sA1'), '1', [1 2 4 3]), ...
         4, getIdentity(rho, 4, '-0', 'sA2'), 1);
     rhoT2 = contract(contract(rho, '14', getIdentity(rho, 1, rho, 4), '12*'), '12', ...
-        getIdentity(rho, 2, rho, 3), '12*');
+        getIdentity(rho, 2, rho, 3), '12*');    
+%     rho = contract(contract(rho, 2, getIdentity(rho, 2, '-0', 'sA2'), '1*', [1 4 2 3]), ...
+%         4, getIdentity(rho, 4, '-0', 'sA2'), 1);
+%     rhoT2 = contract(contract(rho, '14', getIdentity(rho, 1, rho, 4), '12*'), '12', ...
+%         getIdentity(rho, 2, rho, 3), '12*');
+end
+
+function spec = getSpec(m)
+    spec = [];
+    for i = 1:length(m.data)
+        [~, v] = eig(m.data{i});
+        for j = 1:length(v)
+            spec = [spec v(j, j)];
+        end
+    end
 end
