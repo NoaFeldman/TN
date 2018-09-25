c4f8b94ccf8d5eda1d700c2e7edba711bc67dc78
feld.noa@gmail.com <feld.noa@gmail.com>
2018-09-25 18:26:40 +0300

    Negativity

--- MPS/DMRG/partiallyTransposedRDM.m
+++ MPS/DMRG/partiallyTransposedRDM.m
@@ -4,9 +4,8 @@ function rhoT2 = partiallyTransposedRDM(N1, N2)
     M = contract(N1, 2, N2, 1);
     % This action gets me out of memory - divide to sectors
     rho = contract(M, '13', M, '13*');
-    rho.info.itags(3:4) = {'sA1Prime', 'sA2Prime'};
-    rho.Q{3} = -rho.Q{3};
-    rho.Q{4} = -rho.Q{4};
+    rho = contract(contract(rho, 2, getIdentity(rho, 2, '-0', 'sA2'), '1*', [1 4 2 3]), ...
+        4, getIdentity(rho, 4, '-0', 'sA2'), 1);
     rhoT2 = contract(contract(rho, '14', getIdentity(rho, 1, rho, 4), '12*'), '12', ...
         getIdentity(rho, 2, rho, 3), '12*');
 end
