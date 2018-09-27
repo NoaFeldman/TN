91667778b52507e627aa78e6c15bffa47709ace0
NoaFeldman <feld.noa@gmail.com>
2018-09-27 21:55:06 +0300

    varify l so the dependence is also varied...

--- MPS/DMRG/saveNegativities.m
+++ MPS/DMRG/saveNegativities.m
@@ -3,7 +3,7 @@ function saveNegativities(psi, ls, ns, filename)
     for j = 1:length(ls)
         l = ls(j);
         spectrum = containers.Map();
-        [N1, N2] = getNegativityNs(psi, L/2 - l + 1, L/2, L/2 + l);
+        [N1, N2] = getNegativityNs(psi, L/2 - l + 1, L/2, 3*L/4);
         rhoT2 = partiallyTransposedRDM(N1, N2);
         for q = 0:2:12
             spectrum(num2str(q)) = getRenyiNegativity(rhoT2, ns, q);
