be64e6b06d21f0e91dd519f4f0b70b6c804d6fca
feld.noa@gmail.com <feld.noa@gmail.com>
2018-10-14 16:55:32 +0300

    fixing bugs in negativity exact code

--- MPS/exact/chargeRenyi3.m
+++ MPS/exact/chargeRenyi3.m
@@ -1,5 +1,5 @@
 function r = chargeRenyi3(cicj, Q, u1, v1, v2)
-    alphas = -3.141:1e-3:3.141;
+    alphas = -3.14:1e-2:3.14;
     r = 0;
     for alpha = alphas
         r = r + exp(1i * alpha * Q) * fluxRenyi3(cicj, alpha, u1, v1, v2) / (2 * pi);
--- MPS/exact/fluxRenyi3.m
+++ MPS/exact/fluxRenyi3.m
@@ -4,14 +4,14 @@ function r = fluxRenyi3(cicj, alpha, u1, v1, v2)
     u2 = v1+1;
     Gplus = getG(1, cicj, u1, v1, v2);
     Gminus = getG(-1, cicj, u1, v1, v2);
-    N = eye(v2 - u1 + 1); % .* cicj(u1:v2, u1:v2);
+    N = eye(v2 - u1 + 1); 
     I = eye (v2 - u1 + 1);
-    Gpp = (I + Gplus)/2;
-    Gpp2 = Gpp^2;
-    Gpp3 = Gpp2 * Gpp;
-    Gpm = (I - Gplus)/2;
-    Gpm2 = Gpm^2;
-    Gpm3 = Gpm2 * Gpm;
+%     Gpp = (I + Gplus)/2;
+%     Gpp2 = Gpp^2;
+%     Gpp3 = Gpp2 * Gpp;
+%     Gpm = (I - Gplus)/2;
+%     Gpm2 = Gpm^2;
+%     Gpm3 = Gpm2 * Gpm;
 %     Tr = -1/2 * det(Gpm3 + expm(1i .* alpha .* N) * Gpp3) + ...
 %         3/2 * det(Gpm2 * (I - Gminus)/2 + expm(1i .* alpha .* N) * Gpp2 * (I + Gminus) / 2);
     
@@ -21,14 +21,13 @@ function r = fluxRenyi3(cicj, alpha, u1, v1, v2)
     for i = 1:2
         for j= 1:2
             for k = 1:2
-                u =  exp(-1i * pi / 4 * (signs(i) + signs(j) + signs(k))) ;
+                u =  2^(-3/2) * exp(-1i * pi / 4 * (signs(i) + signs(j) + signs(k))) ;
                 Tr = Tr + u * det((I - Gs{i}) * (I - Gs{j}) * (I - Gs{k}) / 2^3 + ...
                     expm(1i .* alpha .* N) * (I + Gs{i}) * (I + Gs{j}) * (I + Gs{k}) / 2^3);
             end
         end
     end
-
-
+    
     L2 = v2 - u2 + 1;
     r = Tr * exp(1i * alpha * L2);
 end
