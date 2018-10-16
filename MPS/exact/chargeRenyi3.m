e52df72aa398aa03de089d873f3705ddc937d2fb
feld.noa@gmail.com <feld.noa@gmail.com>
2018-10-16 12:22:21 +0300

    count for step size in Fourier transform

--- MPS/exact/chargeRenyi3.m
+++ MPS/exact/chargeRenyi3.m
@@ -12,18 +12,24 @@ function r = chargeRenyi3(cicj, Q, u1, v1, v2)
     Gp2Gm = Gp2 * Gminus;
     GpGmGp = GpGm * Gplus;
     L2 = v2 - u2 + 1;
-    alphas = -3.14:1e-2:3.14;
+    stepsize = 1e-2;
+    alphas = -3.14:stepsize:3.14;
     r = 0;
     for alpha = alphas
-        r = r + exp(1i * alpha * Q) / (2 * pi) * ...
+        r = r + exp(-1i * alpha * Q) / (2 * pi / stepsize) * ...
             fluxRenyi3(I, Gplus, Gminus, Gp2, Gp3, GpGm, GmGp, Gp2Gm, GpGmGp, alpha, L2);
     end
 end
 
 function r = fluxRenyi3(I, Gplus, Gminus, Gp2, Gp3, GpGm, GmGp, Gp2Gm, GpGmGp, alpha, L2)
-    Tr = (-1/2 * det((I - 3.*Gplus + 3.*Gp2 - Gp3) / 2^3 + exp(1i .* alpha) .* (I + 3.*Gplus + 3.* Gp2 + Gp3) / 2^3) + ...
+    Tr = -1/2 * det((I - 3.*Gplus + 3.*Gp2 - Gp3) / 2^3 + exp(1i .* alpha) .* (I + 3.*Gplus + 3.* Gp2 + Gp3) / 2^3) + ...
         1 * det((I - 2.*Gplus + Gp2 - Gminus + 2.*GpGm - Gp2Gm) / 2^3 + exp(1i .* alpha) .* (I + 2.*Gplus + Gp2 + Gminus + 2.*GpGm + Gp2Gm) / 2^3) +  ...
-        1/2 * det((I - 2.* Gplus - Gminus + GmGp + Gp2 + GpGm - GpGmGp) / 2^3 +  exp(1i .* alpha) .* (I + 2.* Gplus + Gminus + GmGp + Gp2 + GpGm + GpGmGp) / 2^3));        
+        1/2 * det((I - 2.* Gplus - Gminus + GmGp + Gp2 + GpGm - GpGmGp) / 2^3 +  exp(1i .* alpha) .* (I + 2.* Gplus + Gminus + GmGp + Gp2 + GpGm + GpGmGp) / 2^3);        
+
+%     Tr = -1/2 * det(((I - Gplus) / 2)^3 + exp(1i .* alpha) .* ((I + Gplus) / 2)^3) + ...
+%         1 * det(((I - Gplus) / 2)^2 * ((I - Gminus) / 2) + exp(1i .* alpha) .* ((I + Gplus) / 2)^2 * ((I + Gminus) / 2) ) +  ...
+%         1/2 * det(((I - Gplus) / 2) * ((I - Gminus) / 2) * ((I - Gplus) / 2) + exp(1i .* alpha) .* ((I + Gplus) / 2) * ((I + Gminus) / 2) * ((I + Gplus) / 2));        
    
+
     r = Tr * exp(-1i * alpha * L2);
 end
