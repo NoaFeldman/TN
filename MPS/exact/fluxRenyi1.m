c758a7077bec76b9fda766fcc0f3bd67721a0136
Feldman Noa <feld.noa@gmail.com>
2018-10-21 10:19:04 +0300

    minor

--- MPS/exact/fluxRenyi1.m
+++ MPS/exact/fluxRenyi1.m
@@ -1,5 +1,5 @@
 function r = fluxRenyi1(I, Gplus, Gminus, alpha, L2)
-digits(128);
+% digits(128);
     Tr = 1\sqrt(2) * (exp(-1i * pi / 4) * det((I - Gplus)/2 + exp(1i * alpha) .* (I + Gplus)/2) + ...
                       exp(1i * pi / 4) * det((I - Gminus)/2 + exp(1i * alpha) .* (I + Gminus)/2));
     r = Tr * exp(-1i * alpha * L2);
