2d3455357bb0fde7650eea4414dd5b602b3974af
feld.noa@gmail.com <feld.noa@gmail.com>
2018-12-03 18:57:58 +0200

    negativities based on bosonic vertex

--- MPS/analyzeNegsT.m
+++ MPS/analyzeNegsT.m
@@ -2,42 +2,41 @@
 % clear ws
 % clear epss
 data = load('exactNegQuenchFlux10000_1000-1000_.mat');
-n = 1;
+n = 3;
 K = 1;
 
 l = 1000;
 rn = data.(strcat('r', int2str(n)));
 t = data.t;
-% load('rn3.mat');
+load('rn3.mat');
 
 zeroIndex = find(data.alphas == 0);
-alphaRegion = 200:length(data.alphas) - 200 + 1;
+alphaRegion = 1:length(data.alphas) - 1 + 1;
 
 for i = 1:length(t)
 %     [a, ~, ~, chisq, yfit] = fitnonlin(data.alphas(alphaRegion), data.alphas(alphaRegion), ...
 %         abs(rn(i, alphaRegion)/rn(i, zeroIndex)), 0.01.*ones(length(alphaRegion), 1), ...
 %          0.01.*abs(rn(i, alphaRegion)/rn(i, zeroIndex)), 'fluxNegT', [17 1.1 0.1 0.55], [l n K t(i)], []);
 
-    [a, ~, ~, chisq, yfit] = fitnonlin(data.alphas(alphaRegion), data.alphas(alphaRegion), ...
-        abs(rn(i, alphaRegion)/rn(i, zeroIndex)), 0.01.*ones(length(alphaRegion), 1), ...
-         0.01.*abs(rn(i, alphaRegion)/rn(i, zeroIndex)), 'fluxNeg1stOrder', [17 1.1 0.1], [l n K t(i)], []);
+%     [a, ~, ~, chisq, yfit] = fitnonlin(data.alphas(alphaRegion), data.alphas(alphaRegion), ...
+%         abs(rn(i, alphaRegion)/rn(i, zeroIndex)), 0.01.*ones(length(alphaRegion), 1), ...
+%          0.01.*abs(rn(i, alphaRegion)/rn(i, zeroIndex)), 'fluxNeg1stOrder', [17 1.1], [l n K t(i)], []);
     plot(data.alphas, abs(rn(i, :)/rn(i, zeroIndex))); 
     hold on;
-    plot(data.alphas(alphaRegion), yfit);
+%     plot(data.alphas(alphaRegion), yfit);
     [a, ~, ~, chisq, yfit] = fitnonlin(data.alphas(:), data.alphas(:), ...
         abs(rn(i, :)/rn(i, zeroIndex)), 0.01.*ones(length(data.alphas), 1), ...
-         0.01.*abs(rn(i, :)/rn(i, zeroIndex)), 'fluxNegT', [17 a(2) a(3) 4], [l n K t(i)], []);
+         0.01.*abs(rn(i, :)/rn(i, zeroIndex)), 'fluxNegT', [30 30 0.9], [l n K t(i)], []);
     plot(data.alphas, yfit);
     xlabel('$\alpha$', 'Interpreter', 'latex');
     ylabel(strcat('$R_', int2str(n), '(\alpha)/R_', int2str(n), '$'), 'Interpreter', 'latex');
     title(strcat('$t = ', num2str(t(i)), '$'), 'Interpreter', 'latex');
-%     hold off
+    hold off
     pause(0.1);
     chisqs(i) = chisq;
     epss(i) = a(1);
     nucs(i) = a(2);
-    nucs2(i) = a(3);
-    ws(i) = a(4);
+    ws(i) = a(3);
     r0(i) = sum(rn(i, :)/rn(i, zeroIndex)./(2* pi) * 1e-2);
     r1(i) = sum(exp(-1i.*data.alphas).*rn(i, :)/rn(i, zeroIndex)./(2* pi) * 1e-2);
     r2(i) = sum(exp(-2i.*data.alphas).*rn(i, :)/rn(i, zeroIndex)./(2* pi) * 1e-2);
--- MPS/theo/fluxNegT.m
+++ MPS/theo/fluxNegT.m
@@ -3,36 +3,39 @@ function res = fluxNegT(alpha, a, fixed)
     n = fixed(2);
     K = fixed(3);
     t = fixed(4);
-    a = real(a);
-    epsilon = 30; %a(1);
+    a = abs(a);
+    epsilon = 30; % a(1);
     [w11t, w22t, w33t, w12, w12t, w23, w23t, w13, w13t] = wDiffs(l/2, t, epsilon);
     dw1 = wDerivative(l/2, t, epsilon);    
     dw2 = wDerivative(0, t, epsilon);    
     dw3 = wDerivative(-l/2, t, epsilon);
-    a(2) = abs(a(2));
-    a(3) = abs(a(3)); % abs(a(2)); % 
-    w11t = a(2)^2 * w11t;
-    w22t = a(3)^2 * w22t;
-    w33t = a(2)^2 * w33t;
-    w12 = a(2)*a(3) * w12;
-    w12t = a(2)*a(3) * w12t;
-    w23 = a(2)*a(3) * w23;
-    w23t = a(2)*a(3) * w23t;
-    w13 = a(2)^2 * w13;
-    w13t = a(2)^2 * w13t;
-    dw1 = a(2)^2 * dw1;
-    dw2 = a(3)^2 * dw2;
-    dw3 = a(2)^2 * dw3;
-    wpm = a(4);
+    w11t = a(2) * w11t;
+    w22t = a(2) * w22t;
+    w33t = a(2) * w33t;
+    w12 = a(2) * w12;
+    w12t = a(2) * w12t;
+    w23 = a(2) * w23;
+    w23t = a(2) * w23t;
+    w13 = a(2) * w13;
+    w13t = a(2) * w13t;
+    dw1 = dw1;
+    dw2 = dw2;
+    dw3 = dw3;
+    wpm = a(3);
     
     alpha1 = alpha / (2*pi);
     alpha2 = -2 .* alpha / (2*pi);
     alpha3 = alpha / (2*pi);
-    order1 = (dw1).^(4 * 1/2 * K/n .* alpha1.^2) .* (dw2).^(4 * 1/2 * K/n .* alpha2.^2) .* (dw3).^(4 * 1/2 * K/n .* alpha3.^2) .* ...
-        (w11t).^(-4 * K/n .* alpha1.^2) .* (w22t).^(-4 * K/n .* alpha2.^2) .* (w33t).^(-4 * K/n .* alpha3.^2) .* ...
-        (w12).^(4 * K/n .* alpha1 .* alpha2) .* (w12t).^(-4 * K/n .* alpha1 .* alpha2) .* ...
-        (w23).^(4 * K/n .* alpha2 .* alpha3) .* (w23t).^(-4 * K/n .* alpha2 .* alpha3) .* ...
-        (w13).^(4 * K/n .* alpha1 .* alpha3) .* (w13t).^(-4 * K/n .* alpha1 .* alpha3);
+%     order1 = (dw1).^(4 * 1/2 * K/n .* alpha1.^2) .* (dw2).^(4 * 1/2 * K/n .* alpha2.^2) .* (dw3).^(4 * 1/2 * K/n .* alpha3.^2) .* ...
+%         (w11t).^(-4/2 * K/n .* alpha1.^2) .* (w22t).^(-4/2 * K/n .* alpha2.^2) .* (w33t).^(-4/2 * K/n .* alpha3.^2) .* ...
+%         (w12).^(4 * K/n .* alpha1 .* alpha2) .* (w12t).^(-4 * K/n .* alpha1 .* alpha2) .* ...
+%         (w23).^(4 * K/n .* alpha2 .* alpha3) .* (w23t).^(-4 * K/n .* alpha2 .* alpha3) .* ...
+%         (w13).^(4 * K/n .* alpha1 .* alpha3) .* (w13t).^(-4 * K/n .* alpha1 .* alpha3);
+    order1 = (dw1).^(2 * 1/2 * K/n .* alpha1.^2) .* (dw2).^(2 * 1/2 * K/n .* alpha2.^2) .* (dw3).^(2 * 1/2 * K/n .* alpha3.^2) .* ...
+        (w11t).^(-2/2 * K/n .* alpha1.^2) .* (w22t).^(-2/2 * K/n .* alpha2.^2) .* (w33t).^(-2/2 * K/n .* alpha3.^2) .* ...
+        (w12).^(2 * K/n .* alpha1 .* alpha2) .* (w12t).^(-2 * K/n .* alpha1 .* alpha2) .* ...
+        (w23).^(2 * K/n .* alpha2 .* alpha3) .* (w23t).^(-2 * K/n .* alpha2 .* alpha3) .* ...
+        (w13).^(2 * K/n .* alpha1 .* alpha3) .* (w13t).^(-2 * K/n .* alpha1 .* alpha3);
     order2 = 0;
     order2Shift = [0, 1, -1];
     for i = 1:3
@@ -42,18 +45,25 @@ function res = fluxNegT(alpha, a, fixed)
                     alpha1Curr = alpha1 + order2Shift(i);
                     alpha2Curr = alpha2 + order2Shift(j);
                     alpha3Curr = alpha3 + order2Shift(k);
+%                     order2 = order2 + ...
+%                         (dw1).^(4/2 * K/n .* alpha1Curr.^2) .* (dw2).^(4/2 * K/n .* alpha2Curr.^2) .* (dw3).^(4/2 * K/n .* alpha3Curr.^2) .* ...
+%                         (w11t).^(-4/2 * K/n .* alpha1Curr.^2) .* (w22t).^(-4/2 * K/n .* alpha2Curr.^2) .* (w33t).^(-4/2 * K/n .* alpha3Curr.^2) .* ...
+%                         (w12).^(4 * K/n .* alpha1Curr .* alpha2Curr) .* (w12t).^(-4 * K/n .* alpha1Curr .* alpha2Curr) .* ...
+%                         (w23).^(4 * K/n .* alpha2Curr .* alpha3Curr) .* (w23t).^(-4 * K/n .* alpha2Curr .* alpha3Curr) .* ...
+%                         (w13).^(4 * K/n .* alpha1Curr .* alpha3Curr) .* (w13t).^(-4 * K/n .* alpha1Curr .* alpha3Curr);
                     order2 = order2 + ...
-                        (dw1).^(4/2 * K/n .* alpha1Curr.^2) .* (dw2).^(4/2 * K/n .* alpha2Curr.^2) .* (dw3).^(4/2 * K/n .* alpha3Curr.^2) .* ...
-        (w11t).^(-4 * K/n .* alpha1Curr.^2) .* (w22t).^(-4 * K/n .* alpha2Curr.^2) .* (w33t).^(-4 * K/n .* alpha3Curr.^2) .* ...
-        (w12).^(4 * K/n .* alpha1Curr .* alpha2Curr) .* (w12t).^(-4 * K/n .* alpha1Curr .* alpha2Curr) .* ...
-        (w23).^(4 * K/n .* alpha2Curr .* alpha3Curr) .* (w23t).^(-4 * K/n .* alpha2Curr .* alpha3Curr) .* ...
-        (w13).^(4 * K/n .* alpha1Curr .* alpha3Curr) .* (w13t).^(-4 * K/n .* alpha1Curr .* alpha3Curr);
+                        (dw1).^(2/2 * K/n .* alpha1Curr.^2) .* (dw2).^(2/2 * K/n .* alpha2Curr.^2) .* (dw3).^(2/2 * K/n .* alpha3Curr.^2) .* ...
+                        (w11t).^(-2/2 * K/n .* alpha1Curr.^2) .* (w22t).^(-2/2 * K/n .* alpha2Curr.^2) .* (w33t).^(-2/2 * K/n .* alpha3Curr.^2) .* ...
+                        (w12).^(2 * K/n .* alpha1Curr .* alpha2Curr) .* (w12t).^(-2 * K/n .* alpha1Curr .* alpha2Curr) .* ...
+                        (w23).^(2 * K/n .* alpha2Curr .* alpha3Curr) .* (w23t).^(-2 * K/n .* alpha2Curr .* alpha3Curr) .* ...
+                        (w13).^(2 * K/n .* alpha1Curr .* alpha3Curr) .* (w13t).^(-2 * K/n .* alpha1Curr .* alpha3Curr);
                 end
             end
         end
     end    
 %     res = order1;
 %     res = order1 + wpm .* order2;
-    zeroIndex = length(alpha) / 2 + 1/2;;
+    zeroIndex = length(alpha) / 2 + 1/2;
+    disp((wpm * order2(zeroIndex) + order1(zeroIndex)));
     res = (order1 + wpm .* order2) / (wpm * order2(zeroIndex) + order1(zeroIndex));
 end
