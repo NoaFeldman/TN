5885fd0fa3a610e16bd889ef6544f7db1b96c2e2
feld.noa@gmail.com <feld.noa@gmail.com>
2018-12-22 10:18:27 +0200

    fix WCR approximations

--- MPS/theo/rhoCosTheta.m
+++ MPS/theo/rhoCosTheta.m
@@ -1,4 +1,11 @@
 function res = rhoCosTheta(l, t, epsilon)
     % from (82) https://arxiv.org/pdf/1501.00568.pdf
-    res = max(abs(l), t) .* (1 + epsilon^2 ./ (2.*(max(abs(l), t).^2 - min(abs(l), t).^2)));
+%     res = max(abs(l), t) .* (1 + epsilon^2 ./ (2.*(max(abs(l), t).^2 - min(abs(l), t).^2)));
+    multSign = ones(1, length(t));
+    if l > 0
+        multSign(find(t.^2 > l^2 + epsilon^2)) = multSign(find(t.^2 > l^2 + epsilon^2)).*(-1);
+    else
+        multSign(find(t.^2 < l^2 + epsilon^2)) = multSign(find(t.^2 < l^2 + epsilon^2)).*(-1);
+    end
+    res = -rho(l, t, epsilon).*sqrt(1/2  + multSign.*(epsilon^2 + l.^2 + t.^2)./rho(l, t, epsilon).^2);
 end
--- MPS/theo/rhoSinTheta.m
+++ MPS/theo/rhoSinTheta.m
@@ -1,4 +1,11 @@
 function res = rhoSinTheta(l, t, epsilon)
     % from (82) https://arxiv.org/pdf/1501.00568.pdf
-        res = sign(l) .* 1i .* min(abs(l), t) .* (1 + epsilon^2 ./ (2 .* (min(abs(l), t).^2 - max(abs(l), t).^2)));
+%         res = sign(l) .* 1i .* min(abs(l), t) .* (1 + epsilon^2 ./ (2 .* (min(abs(l), t).^2 - max(abs(l), t).^2)));
+    multSign = ones(1, length(t));
+    if l > 0
+        multSign(find(t.^2 < l^2 + epsilon^2)) = multSign(find(t.^2 < l^2 + epsilon^2)).*(-1);
+    else
+        multSign(find(t.^2 > l^2 + epsilon^2)) = multSign(find(t.^2 > l^2 + epsilon^2)).*(-1);
+    end
+    res = rho(l, t, epsilon).*sqrt(1/2  + multSign.*(epsilon^2 + l.^2 + t.^2)./rho(l, t, epsilon).^2);
 end
